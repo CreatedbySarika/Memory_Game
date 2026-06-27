9c717b1f348c5f99e875e3c473a609aca4c2ec42
CreatedBySarika <sahsarika2005@gmail.com>
2026-06-27 21:34:07 +0100

    shapes

---
+++ draw.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+class draw:
+    def rectangle():
+        forward(100) 
+        right(90)
+        forward(50)  
+        right(90)
+        forward(100)
+        right(90)
+        forward(50)
+    
+
+
+
