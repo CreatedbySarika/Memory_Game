760357b1000cb68cace099169fca26f1fb3c2c9d
CreatedBySarika <sahsarika2005@gmail.com>
2026-06-28 12:17:41 +0100

    colours

--- code.py
+++ code.py
@@ -15,3 +15,16 @@ assert (B_width*B_height)%2==0
 
 xmargin = int (W_width-(B_width*(boxsize+gapsize))/2)
 ymargin = int((W_height-(B_height*(boxsize+gapsize))/2))
+
+# colours
+
+GRAY     =   (100,100,100)
+NAVYBLUE =   ( 60, 60,100)
+WHITE    =   (255,255,255)
+RED      =   (255,  0,  0) 
+GREEN    =   (  0, 255,   0) 
+BLUE     =   (  0,   0, 255) 
+YELLOW   =   (255, 255,   0) 
+ORANGE   =   (255, 128,   0) 
+PURPLE   =   (255,   0, 255) 
+CYAN     =   (  0, 255, 255)
